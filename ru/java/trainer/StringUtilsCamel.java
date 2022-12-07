e904f69b91d38b8de8519a1523c2528981079254
olgamatrakhova <gluhova.olga@gmail.com>
2022-12-07 23:54:18 +0300

    Задание 7 из 28: Java синтаксис: массивы, условные операторы, циклы, ДЗ (14)

---
+++ ru/java/trainer/StringUtilsCamel.java
@@ -0,0 +1,32 @@
+/*Дана строка. Реализуйте метод который бы приводил полученную строку к camel case нотации.
+Если встречаются знаки препинания, иные специальные символы или цифры - они должны быть удалены.
+Пример: входная строка - my camel * case string 1, результат - myCamelCaseString*/
+package ru.java.trainer;
+
+public class StringUtilsCamel {
+    public static String toCamelCase(String input) {
+        String result = "";
+        boolean prNew = false;
+        for (char c : input.toCharArray()) {
+            if (Character.isWhitespace(c)) {
+                prNew = true;
+            } else if (Character.isLetter(c)) {
+                if (prNew) {
+                    if (result.equals("")) result += c;
+                    else result += Character.toUpperCase(c);
+                    prNew = false;
+                } else result += c;
+            }
+        }
+        return result;
+    }
+
+    private StringUtilsCamel() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Строка \"my camel * case string 1\" должна преобразоваться в myCamelCaseString - "
+                + StringUtilsCamel.toCamelCase("- 9 4  755 32    my camel * case string 1"));
+    }
+}
